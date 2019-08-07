ab8578743e28f4acaeceaadd3f8bcdba4f360b8d
Olimjon Berdiev <berdiev.olimjon@gmail.com>
2019-08-06 20:40:00 -0400

    commit

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,6 +3,6 @@ public class AppTest {
         System.out.println("Hello world");
         System.out.println("Hello world");
         System.out.println("Fetched");
-         System.out.println("One more time");
+
       }
 }
